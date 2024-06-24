f636c7799808326aadbbc3432a8cb24cc71c864b
BELLBU\bellb <bellbu@naver.com>
2024-06-24 23:07:40 +0900

    POST/GET API 개발 및 테스트(2024-06-24)

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,14 +1,23 @@
 package com.group.libraryapp.controller.calculator;
 
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
 
     @GetMapping("/add") // GET /add
-    public int addTwoNumbers(int number1, int number2) {
-        return number1 + number2;
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumber(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
     }
 
 }
