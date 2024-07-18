366493c7c31a78589a58bfb8a467a2531e239b0f
BELLBU\bellb <bellbu@naver.com>
2024-07-18 23:44:46 +0900

    2024-07-18

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,36 +4,27 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController // @RestController: @Controller + @ResponseBody(json 형태로 데이터를 반환해 줌)
+@RestController // @RestController: 1.API 진입지점 만들어 줌 / 2.UserController 클래스를 스프링 빈으로 등록시켜 줌 / 3.@Controller + @ResponseBody(json 형태로 데이터를 반환해 줌)
 public class UserController { // Controller: API와 HTTP 담당
 
-    private final UserService userService = new UserService();
-    private final JdbcTemplate jdbcTemplate;  // jdbcTemplate 선언
+    private final UserService userService;
 
-    public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public UserController(UserService userService) {  // UserController가 JdbcTemplate에 의존
+        this.userService = userService;
     }
 
     @PostMapping("/user") // 등록
     public void saveUser(@RequestBody UserCreateRequest request) { // @RequestBody: HTTP 요청의 바디에 담긴 값들을 자바객체로 변환시켜 객체에 저장
-        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge()); // .update(): INSERT, UPDATE, DELETE 쿼리에 사용
+        userService.saveUser(request);
     }
 
     @GetMapping("/user") // 목록보기
     public List<UserResponse> getUsers() {
-        String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> { // 두번째 익명 클래스(익명 객체)를 람다식으로 변환 RowMapper
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
+        return userService.getUsers();
 
         /* 첫번째
         List<UserResponse> responses = new ArrayList<>();
@@ -54,23 +45,26 @@ public class UserController { // Controller: API와 HTTP 담당
             }
         });
         */
+        
+        /* 세번째
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> { // 두번째 익명 클래스(익명 객체)를 람다식으로 변환 RowMapper
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+        */
     }
 
     @PutMapping("/user") // 수정
     public void updateUser(@RequestBody UserUpdateRequest request) { // 객체화
-        userService.updateUser(jdbcTemplate, request);
+        userService.updateUser(request);
     }
 
     @DeleteMapping("/user") // 삭제
     public void deleteUser(@RequestParam String name) {
-        String readSql = "SELECT * FROM user WHERE name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty(); // isEmpty(): 비어있는 경우 true 반환, 비어있지 않은 경우 false 반환
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "DELETE FROM user WHERE name = ?";
-        jdbcTemplate.update(sql, name);
+        userService.deletrUser(name);
     }
 
 
--- src/main/java/com/group/libraryapp/service/user/UserService.java
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -1,20 +1,42 @@
 package com.group.libraryapp.service.user;
 
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.repository.user.UserRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserService { // Service: 분기처리나 로직 담당
 
-    private final UserRepository userRepository = new UserRepository();
+    private final UserRepository userRepository;
 
-    public void updateUser(JdbcTemplate jdbcTemplate, UserUpdateRequest request) {
-        if (userRepository.isUserNotExist(jdbcTemplate, request.getId())) {
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(UserCreateRequest request) {
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers() {
+        return userRepository.getUsers();
+    }
+
+    public void updateUser(UserUpdateRequest request) {
+        if (userRepository.isUserNotExist(request.getId())) {
             throw new IllegalArgumentException();
         }
+        userRepository.updateUserName(request.getName(), request.getId());
+    }
 
-        userRepository.updateUserName(jdbcTemplate, request.getName(), request.getId());
-
+    public void deletrUser(String name) {
+        if (userRepository.isUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+        userRepository.deleteUserName(name);
     }
 
 }
